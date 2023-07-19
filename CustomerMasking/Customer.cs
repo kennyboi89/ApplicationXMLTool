847151af37aea35d80c27d667b49dd269051c348
Ken Nguyen <ken.nguyen@contemi.com>
2023-07-19 15:49:32 +0200

    added steps

---
+++ CustomerMasking/Customer.cs
@@ -0,0 +1,20 @@
+﻿
+
+public class Customer
+{
+    public int UserID { get; set; }
+    public string SocialSecurity { get; set; }
+    public string FirstName { get; set; }
+    public string MiddleNames { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public int AddressID { get; set; }
+    public string Line1 { get; set; }
+    public string Line3 { get; set; }
+    public string Postcode { get; set; }
+    public int CustomerUserHistoryID { get; set; }
+    public int VersionID { get; set; }
+    public string CustomerApplicationXML { get; set; }
+    public bool Exported { get; set; }
+    public DateTime? ExportedDate { get; set; }
+}
--- CustomerMasking/Program.cs
+++ CustomerMasking/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+private readonly int _batchSize = 1000;
+private readonly string _connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=DV_UrzusNO_Main_Agency;Persist Security Info=True;User ID=sa;Password=Sql2005$";
+private readonly string tableName = "TempCustomerRecordsForProfileMasking";
+
+/*
+ 
+ * 1. Set the batch size and connection string constants:
+   - _batchSize = 1000
+   - _connectionString = "Data Source=.\SQLEXPRESS;Initial Catalog=DV_UrzusNO_Main_Agency;Persist Security Info=True;User ID=sa;Password=Sql2005$"
+   - tableName = "TempCustomerRecordsForProfileMasking"
+
+2. Connect to SQL and fetch top 1000 non-processed rows:
+   - Open a SQL connection using the _connectionString
+   - Prepare a SQL query to fetch top 1000 non-processed rows from the tableName
+   - Execute the query to retrieve the data
+   - Close the SQL connection
+
+3. Loop through all rows:
+   - For each row in the fetched data:
+     - Map the data into a Customer object:
+       - Create a new instance of the Customer class
+       - Assign the values from the fetched row to the corresponding properties of the Customer object
+
+     - Update CustomerXML:
+       - Perform the necessary operations on the CustomerApplicationXML property of the Customer object to update it accordingly
+
+     - Mark as processed:
+       - Open a new SQL connection using the _connectionString
+       - Prepare a SQL update query to mark the current row as processed in the tableName
+       - Execute the update query to update the row's status as processed
+       - Close the SQL connection
+
+*/
+
+
+
+
 Console.WriteLine("Hello, World!");
